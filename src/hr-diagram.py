ab4643af5f771690cb52129b5c50632216971505
Michael <michael.totarella@mail.concordia.ca>
2021-03-13 20:25:09 -0500

    Added color to data points, as well as some other aesthetic changes.

--- src/hr-diagram.py
+++ src/hr-diagram.py
@@ -1,25 +1,42 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
-df_raw = pd.read_csv('../database/hygdata_v3.csv', usecols=['absmag', 'spect', 'lum', 'ci'])
-
-
-df_raw = df_raw.sort_values('ci')
-print(df_raw.head())
-
-# fig1 = df_raw.plot(kind='scatter', x='ci', y='absmag', s=1)
-# plt.gca().invert_yaxis() # invert axis so bottom to top is positive to negative values, since the most negative the absolute magnitude value the brighter the star
-# plt.grid()
-# plt.title('absolute magnitude vs. color index')
-# fig1.set_xlim(-0.5, 2)
-
-fig2 = df_raw.plot(kind='scatter', x='ci', y='lum', s=1, logy=True)
-plt.grid()
-plt.title('lum vs. color index')
-fig2.set_xlim(-0.5, 2)
-#fig2.set_ylim(0, 1)
+import matplotlib.colors as colors
+from matplotlib import cm
+import seaborn as sns
 
 
+class MidpointNormalize(colors.Normalize):
+    """
+    Normalise the colorbar so that diverging bars work there way either side from a prescribed midpoint value)
+    Function taken from http://chris35wills.github.io/matplotlib_diverging_colorbar/
 
+    e.g. im=ax1.imshow(array, norm=MidpointNormalize(
+        midpoint=0.,vmin=-100, vmax=100))
+    """
+
+    def __init__(self, vmin=None, vmax=None, midpoint=None, clip=False):
+        self.midpoint = midpoint
+        colors.Normalize.__init__(self, vmin, vmax, clip)
+
+    def __call__(self, value, clip=None):
+        # I'm ignoring masked values and all kinds of edge cases to make a
+        # simple example...
+        x, y = [self.vmin, self.midpoint, self.vmax], [0, 0.5, 1]
+        return np.ma.masked_array(np.interp(value, x, y), np.isnan(value))
+
+
+df = pd.read_csv('../database/hygdata_v3.csv',
+                 usecols=['absmag', 'spect', 'lum', 'ci'])
+
+fig, ax = plt.subplots()
+ax.set_facecolor('black')
+palette = cm.get_cmap("RdYlBu", 12)
+plt.scatter(x="ci", y="lum", data=df.iloc[::15], norm=MidpointNormalize(midpoint=0, vmin=-0.33, vmax=1.4),
+            edgecolor=None, alpha=1, s=5, c="ci", cmap="RdYlBu_r")
+ax.set(yscale="log")
+ax.set_xlim(-0.5, 2.3)
+plt.title("Hertzsprung-Russell Diagram")
+ax.set_xlabel("Color Index")
+ax.set_ylabel("Luminosity")
 plt.show()
